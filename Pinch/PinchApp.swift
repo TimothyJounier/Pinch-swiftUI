85422355c7b8d17410b5d4018e8a8a753951530c
TimothyJounier <timothyjounier@gmail.com>
2023-05-25 08:22:24 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Timothy jounier on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
