2f72120f96aec2fe7da2fe5daa293bfa9e7cb174
TimothyJounier <timothyjounier@gmail.com>
2023-05-31 08:49:33 +0200

    add Animation on image with onAppear

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTY
     
+    @State private var isAnimatin: Bool = false
+    
     // MARK: - FUNCTION
     
     // MARK: - CONTENT
@@ -23,10 +25,16 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x:2, y:2)
+                    .opacity(isAnimatin ? 1 : 0)
                 
             } // : ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)){
+                    isAnimatin = true
+                }
+            })
         } // : NAVIGATION
         .navigationViewStyle(.stack)
     }
