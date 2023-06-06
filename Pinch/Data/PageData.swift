3d8d974c0dc479edf5af1b5346b6b5be7946635d
TimothyJounier <timothyjounier@gmail.com>
2023-06-06 14:45:35 +0200

    Page extension Modification page data Add let pages in contentView

--- Pinch/Data/PageData.swift
+++ Pinch/Data/PageData.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-let pagesdATA: [Page] = [
+let pagesData: [Page] = [
     Page(id: 1, imageName: "magazine-front-cover"),
     Page(id: 2, imageName: "magazine-back-cover")
 ]
