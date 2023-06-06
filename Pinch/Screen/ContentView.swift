7405f513b8b93bf5b29d05094083f23dd07803b3
TimothyJounier <timothyjounier@gmail.com>
2023-06-06 10:25:15 +0200

    add gesture and facto var imaeOffset

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     @State private var isAnimatin: Bool = false
     @State private var imageScale: CGFloat = 1
-    @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
     
@@ -28,6 +28,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x:2, y:2)
                     .opacity(isAnimatin ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
@@ -41,7 +42,23 @@ struct ContentView: View {
                             }
                         }
                     })
-                
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    withAnimation(.spring()){
+                                        imageScale = 1
+                                        imageOffset = .zero
+                                    }
+                                }
+                            }
+                    )
             } // : ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
